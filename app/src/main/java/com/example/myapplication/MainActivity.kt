9a7d6440c9761821256c85b7e5f6f1ae4b6217d7
usha1208 <usharani.s.2019.cse@rajala>
2022-03-08 19:22:40 +0530

    Final commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="utf-8"?>
+<LinearLayout
+xmlns:android="http://schemas.android.com/apk/res/android"
+xmlns:app="http://schemas.android.com/apk/res-auto"
+xmlns:tools="http://schemas.android.com/tools"
+android:layout_width="match_parent"
+android:layout_height="match_parent"
+android:orientation="vertical"
+android:padding="10dp"
+tools:context=".MainActivity">
+
+<TextView
+android:id="@+id/textView3"
+android:layout_width="match_parent"
+android:layout_height="wrap_content"
+android:text="Welcome"
+android:layout_gravity="center_vertical"
+android:textAlignment="center"
+android:layout_marginTop="50dp"
+android:layout_marginBottom="80dp"
+android:textSize="30dp"
+/>
+
+<Button
+android:id="@+id/button2"
+android:layout_width="match_parent"
+android:layout_height="wrap_content"
+android:text="FONT SIZE"
+android:layout_marginBottom="10dp"
+/>
+
+<Button
+android:id="@+id/button3"
+android:layout_width="match_parent"
+android:layout_height="wrap_content"
+android:text="FONT COLOR"
+/>
+
+</LinearLayout>
